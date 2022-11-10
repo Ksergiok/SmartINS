65bf1afa0eb5e13b0471e9fd7393ffb2c504c81e
Sukharev Vladimir <vladimir.sukharev@soarse.com>
2022-11-10 19:43:49 +0300

    1. Use Jupiter (junit 5) tests, do not use 4. 2. Use embedded database for tests if possible. 3. Configure application.properties for test environment. 4. Base package for tests should always match base package for service. 5. When spring-security-test in classpath, use @WithMockuser and apply springSecurity() to WebMvcBuilder. 6. Mock any dependencies when using test slices like @WebMvcTest. 7. It's better do not commit target/ at all - use .gitigore.

---
+++ src/test/java/bg/blkn/smartins/controller/MainControllerTest.java
@@ -0,0 +1,49 @@
+package bg.blkn.smartins.controller;
+
+import bg.blkn.smartins.repos.PolicyRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * @author Ksergi0k
+ */
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(MainController.class)
+public class MainControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @MockBean
+    private PolicyRepo policyRepo;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).apply(springSecurity()).build();
+    }
+
+    @Test
+    @WithMockUser
+    void testGreetingPage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("greeting"))
+                .andExpect(content().string(containsString("Hello")));
+    }
+}
