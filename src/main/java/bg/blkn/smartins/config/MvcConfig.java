13f71270a21e198ded484c0731732e1fcaa097cc
Ksergiok <svsuleymanov@gmail.com>
2022-11-14 22:06:56 +0200

    Merge origin/main into main

--- src/main/java/bg/blkn/smartins/config/MvcConfig.java
+++ src/main/java/bg/blkn/smartins/config/MvcConfig.java
@@ -1,10 +1,12 @@
 package bg.blkn.smartins.config;
 
 import java.time.Duration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -20,7 +22,6 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-
         return builder
                 .setConnectTimeout(Duration.ofMillis(3000))
                 .setReadTimeout(Duration.ofMillis(3000))
