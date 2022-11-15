934ba66147b3c0fff01adb10e565a6dcbc72d2b0
Ksergiok <svsuleymanov@gmail.com>
2022-11-15 23:25:09 +0200

    Added PolicyType table

---
+++ src/main/java/bg/blkn/smartins/domain/PolicyType.java
@@ -0,0 +1,40 @@
+package bg.blkn.smartins.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author Ksergi0k
+ */
+@Entity
+public class PolicyType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    int id;
+           
+//    @OneToOne(mappedBy = "type")
+    private String type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    
+}
