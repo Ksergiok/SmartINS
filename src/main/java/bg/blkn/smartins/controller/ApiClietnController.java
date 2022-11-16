5a65bc6e06f19d5694723bb05bb91277d14ca435
Ksergiok <svsuleymanov@gmail.com>
2022-11-16 20:00:22 +0200

    Object mapping from JSON added

--- src/main/java/bg/blkn/smartins/controller/ApiClietnController.java
+++ src/main/java/bg/blkn/smartins/controller/ApiClietnController.java
@@ -1,5 +1,8 @@
 package bg.blkn.smartins.controller;
 
+import bg.blkn.smartins.domain.RandomFact;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import static org.springframework.data.repository.init.ResourceReader.Type.JSON;
@@ -19,11 +22,16 @@ public class ApiClietnController {
     private RestTemplate restTemplate;
 
     @GetMapping(path = "/apiexample")
-    public String getPokemon(Authentication authentication, Map<String, Object> model) {
+    public String getPokemon(Authentication authentication, Map<String, Object> model) throws JsonProcessingException {
         model.put("username", authentication.getName());
-        String pokemon = restTemplate.getForObject("http://jservice.io/api/random?count=1", String.class);
-        model.put("pokemon", pokemon);
+
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
         
-        return "apiexample";
+        RandomFact[] randomFact = objectMapper.readValue(restTemplate.getForObject("http://jservice.io/api/random?count=1", String.class), RandomFact[].class);
+        model.put("randomFact", randomFact[0]);
+
+        return "api/apiexample";
     }
 }
