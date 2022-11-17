b607f253e012b9320ceda5fd7537a83076279ba6
Ksergiok <svsuleymanov@gmail.com>
2022-11-17 23:53:23 +0200

    Rest authentication adding in progress

--- src/main/java/bg/blkn/smartins/config/WebSecurityConfig.java
+++ src/main/java/bg/blkn/smartins/config/WebSecurityConfig.java
@@ -3,53 +3,67 @@ package bg.blkn.smartins.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class WebSecurityConfig {
 
+//    private final RsaKeyProperties rsaKeys;
+//    public WebSecurityConfig(bg.blkn.smartins.config.RsaKeyProperties rsaKeys) {
+//        this.rsaKeys = rsaKeys;
+//    }
+    
+    
+    
+
     @Autowired
-    private DataSource dataSource;
+    UserDetailsService smartInsUserDetailsService;
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth)
             throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .usersByUsernameQuery("select username, password, active from usr where username=?")
-                .authoritiesByUsernameQuery("select u.username, ur.roles from usr u inner join user_role ur on u.id = ur.user_id where u.username=?");
+        auth.userDetailsService(smartInsUserDetailsService).passwordEncoder(
+                NoOpPasswordEncoder.getInstance());
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-        }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((requests) -> requests
-                        .antMatchers("/css/**").permitAll()
-                        .antMatchers("/registration").hasRole("ADMIN")
-                        .anyRequest()
-                        .authenticated())
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .permitAll()
+                .authorizeRequests((requests) -> requests
+                    .antMatchers("/css/**").permitAll()
+                    .antMatchers("/api/v1/login").permitAll()
+   //                 .antMatchers("/registration").hasRole("ADMIN") //Replaced with @PreAuthorize annotation on methods
+                    .anyRequest().authenticated()
                 )
+                .formLogin((form) -> form
+                    .loginPage("/login").permitAll()
+                )
+//                .httpBasic(Customizer.withDefaults())
+//                .userDetailsService(smartInsUserDetailsService)
+//                  .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .logout(LogoutConfigurer::permitAll);
 
         return http.build();
     }
+    
+//    @Bean
+//    JwtDecoder jwtDecoder(){
+//        return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+//    }
 }
